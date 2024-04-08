dbf68a0a45fc28f1ded0abcc0ee0f81aad5c5e5b
Smile-92 <166427800+Smile-92@users.noreply.github.com>
2024-04-08 23:53:14 +0545

    Update run.py

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 import os,time
 print(f' Checking Updates')
-os.system('git pull')
+os.system('git pull -q')
 os.system('xdg-open https://chat.whatsapp.com/LpA8RKW3MU409riyzSIqKC')
 os.system("python SMILE")
